01bfb230cb0b6050a74e73cb290f20c2fa602fd3
Victor <victorshunko@mail.ru>
2023-04-11 23:50:41 +0300

    add function to count sentences

--- lab2/reg_exp_patterns.py
+++ lab2/reg_exp_patterns.py
@@ -2,3 +2,13 @@ SENTENCE = r'[.!\?]+'
 NON_DECLARATIVE_SENTENCE = r'[!\?]+'
 WORD = r'\b[a-zA-Z\d]+\b'
 NUMBER = r'\b\d+\b'
+
+SINGLE_WORD_ABBREVIATION = [
+    'etc.', 'vs.', 'jr.', 'sr.', 'mr.', 'ms.', 'mrs.', 'smb.', 'smth.', 'adj.', 'prep.', 'pp.', 'par.', 'ex.',
+    'pl.', 'edu.', 'appx.', 'sec.', 'gm.', 'cm.', 'yr.', 'jan.', 'feb.', 'mar.',
+    'apr.', 'jun.', 'jul.', 'aug.', 'sep.', 'oct.', 'nov.', 'dec.', 'mon.', 'tue.', 'wed.', 'thu', 'fri.', 'sat.',
+    'sun.']
+
+TWO_WORD_ABBREVIATIONS = ['e.g.', 'i.e.', 'p.s.']
+
+THREE_WORD_ABBREVIATIONS = ['v.i.p.', 'p.p.s.']
