698ead3c5a10d7d374c0868a88e91a6d5358c411
Victor <victorshunko@mail.ru>
2023-04-13 11:20:38 +0300

    add function for average length

--- lab2/task1/statistic_of_text.py
+++ lab2/task1/statistic_of_text.py
@@ -1,5 +1,6 @@
 import re
-from task1.reg_exp_patterns import SENTENCE, NON_DECLARATIVE_SENTENCE, SINGLE_WORD_ABBREVIATION, TWO_WORD_ABBREVIATIONS
+from task1.reg_exp_patterns import SENTENCE, NON_DECLARATIVE_SENTENCE, SINGLE_WORD_ABBREVIATION, \
+    TWO_WORD_ABBREVIATIONS, NUMBER, WORD
 
 
 def count_sentences(text: str) -> int:
@@ -16,4 +17,17 @@ def count_sentences(text: str) -> int:
 
 
 def count_non_declarative_sentences(text: str) -> int:
-    return len(re.findall(NON_DECLARATIVE_SENTENCE, text.lower()))
+    return len(re.findall(NON_DECLARATIVE_SENTENCE, text))
+
+
+def get_avg_sentence_len(text: str) -> float:
+    nums = re.findall(NUMBER, text)
+    words = [word for word in re.findall(WORD, text) if word not in nums]
+    words_len = sum(len(word) for word in words)
+    return round(words_len / count_sentences(text), 2) if count_sentences(text) != 0 else 0
+
+
+def get_avg_word_len(text: str) -> float:
+    words = re.findall(WORD, text)
+    words_len_in_characters = sum(len(word) for word in words)
+    return round(words_len_in_characters / len(words), 2) if len(words) != 0 else 0
--- lab2/task1/test_count.py
+++ lab2/task1/test_count.py
@@ -4,35 +4,78 @@ import statistic_of_text
 
 class TestCount(unittest.TestCase):
     def test_empty_text(self):
-        self.assertEqual(statistic_of_text.count_sentences(''), 0, 'Empty text testing: the count of sentences isn\'t '
-                                                                   'equals to zero')
+        expected = 0
+        self.assertEqual(statistic_of_text.count_sentences(''), expected,
+                         'Empty text test: result isn\'t ' + str(expected))
 
     def test_abbreviations(self):
         text = 'Good evening Mrs. Abramson. It is sample text i.e. and more Ph.D. - it\'s great text!!'
-        self.assertEqual(statistic_of_text.count_sentences(text), 2, 'Abbreviations test: the number of sentences'
-                                                                     'does not equal to 2')
+        expected = 2
+        self.assertEqual(statistic_of_text.count_sentences(text), expected,
+                         'Abbreviations test: number of sentences not ' + str(expected))
 
     def test_sample_text(self):
         text = 'Hey!!! That\'s a good idea; Oh no... I have forgot my keys in the car. '
-        self.assertEqual(statistic_of_text.count_sentences(text), 3, 'Text test: the number of sentences does not '
-                                                                     'equal to 3')
+        expected = 3
+        self.assertEqual(statistic_of_text.count_sentences(text), expected,
+                         'Sample text test: number of sentences not ' + str(expected))
 
 
 class TestNonDeclarative(unittest.TestCase):
     def test_empty_text(self):
-        self.assertEqual(statistic_of_text.count_non_declarative_sentences(''), 0,
-                         'Empty text test: result isn\'t zero')
+        expected = 0
+        self.assertEqual(statistic_of_text.count_non_declarative_sentences(''), expected,
+                         'Empty text test: result isn\'t ' + str(expected))
 
     def test_abbreviations(self):
         text = 'Good evening Mrs. Abramson. It is sample text i.e. and more Ph.D. - it\'s great text!!'
-        self.assertEqual(statistic_of_text.count_non_declarative_sentences(text), 1, 'Abbreviations test: the number of sentences'
-                                                                           'does not equal to 1')
+        expected = 1
+        self.assertEqual(statistic_of_text.count_non_declarative_sentences(text), expected,
+                         'Abbreviations test: number of non decl. sentences not' + str(expected))
 
     def test_sample_text(self):
         text = 'Hey!!! That\'s a good idea; Oh no... I have forgot my keys in the car. '
-        self.assertEqual(statistic_of_text.count_non_declarative_sentences(text), 1,
-                         'Text test: the number of sentences does not '
-                         'equal to 1')
+        expected = 1
+        self.assertEqual(statistic_of_text.count_non_declarative_sentences(text), expected,
+                         'Sample text test: number of non decl. sentences not ' + str(expected))
+
+
+class TestGetAverageSentenceLength(unittest.TestCase):
+    def test_empty_text(self):
+        expected = 0
+        self.assertEqual(statistic_of_text.get_avg_sentence_len(''), expected,
+                         'Empty text test: result isn\'t ' + str(expected))
+
+    def test_abbreviations(self):
+        text = 'Hello, Mr. Derek. Sample text i.e. and more Ph.D. - it\'s greate text!!'
+        expected = round(47 / 2, 2)
+        self.assertEqual(statistic_of_text.get_avg_sentence_len(text), expected,
+                         'Average len with abbreviations test: result isn\'t ' + str(expected))
+
+    def test_text_with_numbers(self):
+        text = 'Oh!!! 4 Heloo aaaa. asd6laa, 777; word?'
+        expected = round(22 / 3, 2)
+        self.assertEqual(statistic_of_text.get_avg_sentence_len(text), expected,
+                         'Average len with numbers test: result isn\'t ' + str(expected))
+
+
+class TestAvgWordLength(unittest.TestCase):
+    def test_empty_text(self):
+        expected = 0
+        self.assertEqual(statistic_of_text.get_avg_word_len(''), expected,
+                         'Empty text test: result isn\'t ' + str(expected))
+
+    def test_abbreviations(self):
+        text = 'Hello, Mr. Derek. Sample text i.e. and more Ph.D. - its greate text!!'
+        expected = round(47 / 14, 2)
+        self.assertEqual(statistic_of_text.get_avg_word_len(text), expected,
+                         'Average len with abbreviations test: result isn\'t ' + str(expected))
+
+    def test_text_with_numbers(self):
+        text = 'Oh!!! 4 Heloo aaaa. asd6laa, 777; word?'
+        expected = round(26 / 7, 2)
+        self.assertEqual(statistic_of_text.get_avg_word_len(text), expected,
+                         'Average len with numbers test: result isn\'t ' + str(expected))
 
 
 if __name__ == '__main__':
